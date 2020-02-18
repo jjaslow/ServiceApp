2f7a7c98a388781057df46b9b93bac0d429c4be3
Jason Jaslow <jason@jaslow.com>
2020-02-18 16:54:28 -0500

    scriptable objects

---
+++ Assets/Zoo App/Scripts/CardPanel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardPanel : MonoBehaviour
+{
+    [SerializeField]
+    Text titleText;
+    [SerializeField]
+    Text bodyText;
+    [SerializeField]
+    Text footerText;
+    [SerializeField]
+    Image animalPhoto;
+    [SerializeField]
+    CardModel[] cards;
+
+    public void openCard(int cardNumber)
+    {      
+        CardModel myCard = cards[cardNumber];
+
+        titleText.text = myCard.title;
+        bodyText.text = myCard.description;
+        footerText.text = myCard.footer;
+        animalPhoto.sprite = myCard.image;
+
+        this.gameObject.SetActive(true);
+    }
+
+    public void closeCard()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+}
---
+++ Assets/Zoo App/Scripts/Scriptable Objects/CardModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName ="New Card", menuName = "Cards")]
+public class CardModel : ScriptableObject
+{
+
+    public string title;
+    public string description;
+    public string footer;
+    public Sprite image;
+}
